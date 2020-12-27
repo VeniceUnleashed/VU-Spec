23d7220df032287764a39e81e59c6ebf98fca2c7
Nikos Kapraras <iamnikosk93@gmail.com>
2020-12-27 18:33:20 +0200

    a proper fix for the objectives bar not updating

--- ui/src/components/MatchBar/ObjectiveBar.js
+++ ui/src/components/MatchBar/ObjectiveBar.js
@@ -7,25 +7,24 @@ export default class ObjectiveBar extends Component
 {
     shouldComponentUpdate(nextProps)
     {
-        // let objectives = this.props.objectives || [];
-        // let nextObjectives = nextProps.objectives || [];
+        let objectives = this.props.objectives || [];
+        let nextObjectives = nextProps.objectives || [];
 
-        // if (objectives.length !== nextObjectives.length)
-        //     return true;
+        if (objectives.length !== nextObjectives.length)
+            return true;
 
-        // for (let i = 0; i < objectives.length; ++i)
-        // {
-        //     let objective = this.props.objective || {};
-        //     let nextObjective = nextProps.objective || {};
+        for (let i = 0; i < objectives.length; ++i)
+        {
+            let objective = objectives[i] || {};
+            let nextObjective = nextObjectives[i] || {};
 
-        //     if (objective.currentTeam !== nextObjective.currentTeam ||
-        //         objective.contested !== nextObjective.contested ||
-        //         objective.label !== nextObjective.label)
-        //         return true;
-        // }
+            if (objective.currentTeam !== nextObjective.currentTeam ||
+                objective.contested !== nextObjective.contested ||
+                objective.label !== nextObjective.label)
+                return true;
+        }
 
-        // return false;
-        return true
+        return false;
     }
 
     render()
